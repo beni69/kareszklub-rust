4ab8ca2298b4f8be70145ae1525d536c0c3ac42c
beni69 <73666440+beni69@users.noreply.github.com>
2022-03-18 22:00:52 +0100

    add closures and mutable references

--- basics/src/functions.rs
+++ basics/src/functions.rs
@@ -1,5 +1,9 @@
 // functions
 // note: no ; at the end of a statement means return
+// closures: inline functions
+// |arg1, arg2| arg1 + arg2
+// functions can be passed as arguments or returned from other functions
+// generics: use the same function for different types
 
 pub fn run() {
     greeting("hello", "tom");
@@ -10,7 +14,21 @@ pub fn run() {
     // closure
     let n3: i32 = 10;
     let add_nums = |n1: i32, n2: i32| n1 + n2 + n3; // access to outside variables
-    println!("closure sum: {}", add_nums(3, 3));
+    println!("sum +10: {}", add_nums(3, 3));
+
+    let nums = [1, 2, 3, 4, 5];
+    let mut sum = 0;
+    for_each(&nums, &mut |n| sum += n); // fn has to be mutable to mutate outside variables
+    println!("array sum: {}", sum);
+
+    let mut count = 0;
+    {
+        let mut c = closure_count(&mut count);
+        for _ in 1..3 {
+            c();
+        }
+    }
+    println!("closure count: {}", count);
 }
 
 fn greeting(greet: &str, name: &str) {
@@ -19,4 +37,16 @@ fn greeting(greet: &str, name: &str) {
 
 fn add(n1: i32, n2: i32) -> i32 {
     n1 + n2 // no ";" means return this value
+}
+
+fn for_each<T>(arr: &[T], callback: &mut dyn FnMut(&T)) {
+    for item in arr {
+        callback(item);
+    }
+}
+
+fn closure_count<'a>(count: &'a mut i32) -> impl FnMut() + 'a {
+    return move || {
+        *count += 1;
+    };
 }
--- basics/src/references.rs
+++ basics/src/references.rs
@@ -5,11 +5,23 @@ pub fn run() {
     let arr1 = [1, 2, 3];
     let arr2 = arr1;
 
-    println!("{:?}", (arr1, arr2)); // works, primitive values copy, don't move
+    println!("{:?}", (arr1, arr2));
+    // works, primitive values copy, don't move
 
     // dynamically sized array
     let vec1 = vec![1, 2, 3];
     let vec2 = &vec1;
 
     println!("{:?}", (&vec1, vec2)); // works
+
+    let vec3 = vec2;
+    println!("{:?}", (&vec1, vec2, vec3));
+    // also works, the reference itself is a primitive value
+
+    // mutable references
+    // either infinite immutable or one mutable reference is allowed
+    let mut num = 5;
+    let num_ref = &mut num;
+    *num_ref += 1; //. * -> dereference operator
+    assert_eq!(num, 6);
 }
